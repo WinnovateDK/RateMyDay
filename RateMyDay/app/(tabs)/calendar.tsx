22dd945c7098b86e0800c99f6c30737df12c3617
tromborg <64638583+tromborg@users.noreply.github.com>
2024-11-20 16:54:33 +0100

    some work on the calendar page

--- RateMyDay/app/(tabs)/calendar.tsx
+++ RateMyDay/app/(tabs)/calendar.tsx
@@ -1,7 +1,6 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import { Calendar, CalendarList } from "react-native-calendars";
 import StatisticsBox from "@/components/StatisticsBox";
-import "../../global.css";
 
 const calendar = () => {
   const colors = [
@@ -35,42 +34,51 @@ const calendar = () => {
     },
   };
   return (
-    <View className="flex-1 bg-white px-4 py-6">
-      <View className="h-1/6 mb-4 justify-center items-center">
-        <Text className="text-xl font-bold text-gray-800">
+    <SafeAreaView className="flex-1 bg-blue-300">
+      {/* Top Section */}
+      <View className="h-1/6 justify-center items-center pt-10">
+        <Text className="text-4xl font-bold text-gray-500">
           Overview of your days
         </Text>
-        <Text className="text-sm text-gray-500">
-          Track how you days have been
+        <Text className="text-xl text-gray-300">
+          Track how your days have been
         </Text>
       </View>
-      <View className="flex-1">
-        <Calendar
-          markedDates={markedDates}
-          theme={{
-            calendarBackground: "white",
-            textSectionTitleColor: "#b6c1cd",
-            selectedDayBackgroundColor: "#00adf5",
-            selectedDayTextColor: "#ffffff",
-            todayTextColor: "#00adf5",
-            dayTextColor: "#2d4150",
-            dotColor: "red",
-          }}
-          style={{
-            borderRadius: 10,
-            overflow: "hidden",
-            elevation: 3,
-            shadowColor: "#000",
-            shadowOffset: { width: 0, height: 2 },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
-            marginVertical: 10,
-          }}
-        />
-      </View>
 
-      <StatisticsBox averageRating={6.4} lowestRating={1} highestRating={10} />
-    </View>
+      <View className="flex-1 ">
+        <View className="flex-1/2 px-2">
+          <Calendar
+            markedDates={markedDates}
+            theme={{
+              calendarBackground: "white",
+              textSectionTitleColor: "#b6c1cd",
+              selectedDayBackgroundColor: "#00adf5",
+              selectedDayTextColor: "#ffffff",
+              todayTextColor: "#00adf5",
+              dayTextColor: "#2d4150",
+              dotColor: "red",
+            }}
+            style={{
+              borderRadius: 10,
+              overflow: "hidden",
+              elevation: 3,
+              shadowColor: "#000",
+              shadowOffset: { width: 0, height: 2 },
+              shadowOpacity: 0.25,
+              shadowRadius: 3.84,
+              marginVertical: 10,
+            }}
+          />
+        </View>
+        <View className="flex-1 mt-4 max-h-full">
+          <StatisticsBox
+            averageRating={6.4}
+            lowestRating={1}
+            highestRating={10}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- RateMyDay/components/StatisticsBox.tsx
+++ RateMyDay/components/StatisticsBox.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 
 type Stats = {
   averageRating: number;
@@ -12,22 +13,46 @@ const StatisticsBox = ({
   highestRating,
   lowestRating,
 }: Stats) => {
+  const [selectedPeriod, setSelectedPeriod] = useState("monthly");
+
   return (
-    <View className="mt-4 p-4 rounded-lg border-2 border-gray-200 max-w-full max-h-full justify-center items-center">
-      <Text className="text-xl font-semibold text-gray-800">Your stats</Text>
-      <View className="mt-2">
-        <Text className="text-sm text-gray-600">
-          Average Rating:{" "}
-          <Text className="font-bold text-gray-900">{averageRating}</Text>
-        </Text>
-        <Text className="text-sm text-gray-600">
-          Highest Rating:{" "}
-          <Text className="font-bold text-gray-900">{highestRating}</Text>
-        </Text>
-        <Text className="text-sm text-gray-600">
-          Lowest Rating:{" "}
-          <Text className="font-bold text-gray-900">{lowestRating}</Text>
-        </Text>
+    <View className="flex-1 px-4 py-3.5 rounded-t-3xl shadow-gray-800 max-w-full max-h-full items-center bg-white">
+      <View className="absolute top-0 right-0">
+        <Picker
+          selectedValue={selectedPeriod}
+          onValueChange={(itemValue) => setSelectedPeriod(itemValue)}
+          style={{ width: 140, height: 40 }}
+        >
+          <Picker.Item label="Weekly" value="weekly" />
+          <Picker.Item label="Monthly" value="monthly" />
+          <Picker.Item label="Yearly" value="yearly" />
+        </Picker>
+      </View>
+
+      <View className="w-full items-center">
+        <Text className="text-xl font-semibold text-gray-800">Your stats</Text>
+      </View>
+      <View className="justify-center items-center h-full">
+        <View className="flex items-center">
+          <Text className="text-4xl text-gray-600">Average Rating</Text>
+          <Text className="text-4xl font-bold text-gray-900 mt-4">
+            {averageRating}
+          </Text>
+        </View>
+        <View className="mt-5 flex-row justify-between w-full px-4">
+          <View className="flex items-center">
+            <Text className="text-xl text-gray-600">Lowest Rating</Text>
+            <Text className="text-2xl font-bold text-gray-900 mt-4">
+              {lowestRating}
+            </Text>
+          </View>
+          <View className="items-center">
+            <Text className="text-xl text-gray-600">Highest Rating</Text>
+            <Text className="text-2xl font-bold text-gray-900 mt-4">
+              {highestRating}
+            </Text>
+          </View>
+        </View>
       </View>
     </View>
   );
