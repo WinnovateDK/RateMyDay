024220eaafda090b8315f15877633cd5c0333193
dulda280 <Sebber280@live.dk>
2025-04-17 21:02:58 +0200

    package upgrades

--- RateMyDay/app/_layout.tsx
+++ RateMyDay/app/_layout.tsx
@@ -1,31 +1,51 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { DarkTheme, DefaultTheme, NavigationContainer, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Stack, useRouter } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect, useState } from "react";
-import "react-native-reanimated";
+import { useEffect } from "react";
 import { useColorScheme } from "@/hooks/useColorScheme";
-import Toast, { ErrorToast } from "react-native-toast-message";
+import Toast, { BaseToast, ErrorToast, ToastConfig } from "react-native-toast-message";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+export {
+  ErrorBoundary,
+} from "expo-router";
+
+export const unstable_settings = {
+  initialRouteName: "(tabs)",
+};
+
+const toastConfig: ToastConfig = {
+  success: (props) => (
+    <BaseToast
+      {...props}
+      style={{ borderLeftColor: "#4CAF50" }}
+      contentContainerStyle={{ paddingHorizontal: 15 }}
+      text1Style={{
+        fontSize: 15,
+        fontWeight: "400",
+      }}
+    />
+  ),
+  error: (props) => (
+    <ErrorToast
+      {...props}
+      style={{ borderLeftColor: "#FF0000" }}
+      text1Style={{
+        fontSize: 15,
+        fontWeight: "400",
+      }}
+      text2Style={{
+        fontSize: 13,
+      }}
+    />
+  ),
+};
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
-  const router = useRouter();
-
-  const toastConfig = {
-    error: (props: any) => (
-      <ErrorToast {...props} style={{ borderLeftColor: "#bae6fd" }} />
-    ),
-  };
 
   useEffect(() => {
     if (loaded) {
@@ -33,16 +53,17 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+  if (!loaded) return null;
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ title: "Main App" }} />
-      </Stack>
-      <Toast config={toastConfig} />
-    </ThemeProvider>
+    <NavigationContainer independent={false}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          
+        </Stack>
+        <Toast config={toastConfig} />
+      </ThemeProvider>
+    </NavigationContainer>
   );
 }
